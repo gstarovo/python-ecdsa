dfbb2cc924b32c8e93139003a5a490fffb52c547
Ganna Starovoytova <gstarovo@redhat.com>
2025-09-04 12:15:52 +0200

    try download newer version of git

--- src/ecdsa/_version.py
+++ src/ecdsa/_version.py
@@ -367,7 +367,7 @@ def git_pieces_from_vcs(tag_prefix, root, verbose, runner=run_command):
     else:
         # HEX: no tags
         pieces["closest-tag"] = None
-        count_out, rc = runner(GITS, ["rev-list", "--count", "HEAD"], cwd=root)
+        count_out, rc = runner(GITS, ["rev-list", "HEAD", "--count"], cwd=root)
         pieces["distance"] = int(count_out)  # total number of commits
 
     # commit date: see ISO-8601 comment in git_versions_from_keywords()
